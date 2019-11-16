a661022ea2bb9b1f0be883838efebd4e3a3d95d5
Buddy Agyin <buddyagyin@Buddys-MacBook-Pro.local>
2019-11-15 19:02:04 -0700

    add removeItem and useLocalStorage

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from './contexts/ProductContext.js';
 import CartContext from './contexts/CartContext.js';
+import useLocalStorage from './hooks/useLocalSotarge.js';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useLocalStorage('cart', []);
 
 	const addItem = item => {
 		// add the given item to the cart
 		setCart(cart => [...cart, item]);
+		console.log(cart);
 	};
 
+	const removeItem = props => {
+		const filteredCart = cart.filter(item => {
+			return item.id !== props
+		})
+		setCart(filteredCart);
+	}
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={{ cart }}>
+				<CartContext.Provider value={{ cart, removeItem }}>
 					<Navigation/>
 
 					{/* Routes */}
